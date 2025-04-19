e3417bb33fb5f393c7f5bf0e9328217f2cc6d696
SUMIT DHABALDEB <122662648+ssvillan@users.noreply.github.com>
2025-04-19 17:49:39 +0530

    Update test_file_writer.py

--- test_file_writer.py
+++ test_file_writer.py
@@ -1,21 +1,13 @@
-import unittest
 import os
 from file_writer import write_to_file
 
-class TestFileWriter(unittest.TestCase):
-    def test_write_to_file(self):
-        test_filename = 'test_output.txt'
-        test_text = 'Hello, GitHub Actions!'
-        
-        write_to_file(test_filename, test_text)
-        
-        self,assertTrue(os.path.exists(test_filename)),"File was not created"
-        
-        with open(test_filename, 'r') as f:
-            content = f.read()
-            self.assertEqual(content, test_text),"File content do not match"
-            os.remove(test_filename)
+def test_write_to_file();
+    test_filename="test_output.txt"
+    test_content="Test Content for CI"
+    write_to_file(test_filename,test_content)
 
-if __name__ == '__main__':
-    unittest.main()
-    
+    assert os.path.exists(test_filename),"File was not created"
+
+    with open(test_filename,"r") as f:
+        content= f.read()
+        assert content == test_content,"File content do not match"
