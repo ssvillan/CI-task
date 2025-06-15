861702785f5a6c9a21c3c16268b9162f5b9c9299
SUMIT DHABALDEB <122662648+ssvillan@users.noreply.github.com>
2025-06-15 16:03:38 +0530

    Delete file_writer.py

--- file_writer.py
+++
@@ -1,6 +0,0 @@
-def write_to_file(filename,content):
-    with open(filename,"w") as f:
-        f.write(content)
-if __name__ == "__main__":
-    write_to_file("output.txt","Hello From Github Actions")
-    print("File Created and Content Written.")
